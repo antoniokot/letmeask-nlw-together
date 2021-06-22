eee9047164502973569f3a757acf1feba675a972
= <55291669+antoniokot@users.noreply.github.com>
2021-06-22 10:53:02 -0300

    Add Google authentication with firebase services

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { Button } from './components/Button';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { auth, firebase } from "./services/firebase";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
+
   return (
-    <div>
-      <Button text="Clique aqui" />
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
